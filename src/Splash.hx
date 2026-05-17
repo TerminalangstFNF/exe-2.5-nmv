45ed11027134c5809259030546f17b24811e6c11
kelsey <terminalangst@proton.me>
2026-05-17 19:56:26 +0100

    init commit

---
+++ src/Splash.hx
@@ -0,0 +1,58 @@
+package;
+
+import funkin.FunkinAssets;
+#if VIDEOS_ALLOWED import funkin.video.FunkinVideoSprite; #end
+
+@:access(flixel.FlxGame)
+class Splash extends flixel.FlxState {
+    var _cachedAutoPause:Bool;
+    
+    #if VIDEOS_ALLOWED video:FunkinVideoSprite;#end
+
+    override function create() {
+        super.create();
+        
+        _cachedAutoPause = FlxG.autoPause = false;
+
+        FlxTimer.wait(1, () -> start());
+    }
+
+    function start() {
+        #if VIDEOS_ALLOWED 
+            add(video = new FunkinVideoSprite());
+            video.onFormat(() -> {
+                video.setGraphicSize(0, FlxG.height);
+                video.updateHitbox();
+                video.screenCenter();
+            });
+            video.onEnd(finish);
+            if (video.load(Paths.video('intro'))) video.delayAndStart();
+			else
+        #end
+    }
+
+    override function update(elapsed:Float) {
+        #if VIDEOS_ALLOWED 
+            if (video != null) {
+                if (FlxG.keys.justPressed.SPACE || FlxG.keys.justPressed.ENTER)
+                    finish();
+            }
+        #end
+
+        super.update(elapsed);
+    }
+
+    function finish() {
+        #if VIDEOS_ALLOWED 
+            video.stop();
+            video.destroy();
+        #end
+
+        FlxG.sound.muted = FlxG.save.data.muted;
+        FlXG.sound.volume = FlxG.save.data.volume;
+
+        FlxG.autoPause = _cachedAutoPause;
+
+        FlxG.switchState(() -> Type.createInstance(Main.startMeta.initialState, []));
+    }
+}
